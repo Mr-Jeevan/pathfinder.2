c265e066acfe77ca32978760d965e02b4c3c6c9a
Mr-Jeevan <jeevanraj.rj7@gmail.com>
2026-02-11 17:35:09 +0530

    cors fix 1

--- pathfinder-backend/server.js
+++ pathfinder-backend/server.js
@@ -17,7 +17,7 @@ const errorHandler = require('./middleware/errorHandler'); // New Enhancement
 const app = express();
 
 // Standard Middleware
-CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
 app.use(cors({ origin: CLIENT_URL }));
 app.use(express.json());
 
